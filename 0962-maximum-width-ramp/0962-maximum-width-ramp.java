859fd9f7fed3e31aa929a0c4df5c798da4d0b395
Aarin1 <arinpatel999@gmail.com>
2026-09-10 17:20:56 +0530

    Time: 3 ms (100%), Space: 59.3 MB (91.65%) - LeetHub

---
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        int[] stack = new int[n];
+        int top = -1;
+        
+        for (int i = 0; i < n; i++) {
+            if (top == -1 || nums[stack[top]] > nums[i]) {
+                stack[++top] = i;
+            }
+        }
+        
+        int maxWidth = 0;
+        
+        for (int j = n - 1; j >= 0; j--) {
+            while (top >= 0 && nums[stack[top]] <= nums[j]) {
+                maxWidth = Math.max(maxWidth, j - stack[top]);
+                top--; 
+            }
+        }
+        
+        return maxWidth;
+    }
+}
